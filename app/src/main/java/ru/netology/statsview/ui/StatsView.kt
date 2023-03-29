d04a30b87a4b3dff455e68b5f5f1b7749b7cb41f
Andrei Borisov <clinri@ya.ru>
2023-03-29 22:34:38 +0300

    paint circle

---
+++ app/src/main/java/ru/netology/statsview/ui/StatsView.kt
@@ -0,0 +1,43 @@
+package ru.netology.statsview.ui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
+import android.util.AttributeSet
+import android.view.View
+import ru.netology.statsview.utils.AndroidUtils
+import kotlin.math.min
+
+class StatsView @JvmOverloads constructor(
+    context: Context,
+    attributeSet: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0,
+) : View(
+    context,
+    attributeSet,
+    defStyleAttr,
+    defStyleRes
+) {
+    private var radius = 0F
+    private var center = PointF()
+    private var lineWidth = AndroidUtils.dp(context, 5)
+    private val paint = Paint(
+        Paint.ANTI_ALIAS_FLAG
+    ).apply {
+        strokeWidth = lineWidth.toFloat()
+        style = Paint.Style.STROKE
+        strokeJoin = Paint.Join.ROUND
+        strokeCap = Paint.Cap.ROUND
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(w, h) / 2F - lineWidth
+        center = PointF(w / 2F, h / 2F)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawCircle(center.x, center.y, radius, paint)
+    }
+}
