6cae227a1e362a7760d563af8081bc02b22b48c1
Andrei Borisov <clinri@ya.ru>
2023-04-02 22:23:04 +0300

    code from lecture 06_animations. Implement Scale animation

--- app/src/main/java/ru/netology/statsview/MainActivity.kt
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -2,18 +2,25 @@ package ru.netology.statsview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import ru.netology.statsview.ui.StatsView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<StatsView>(R.id.statsView).data = listOf(
+        val view = findViewById<StatsView>(R.id.statsView)
+        view.data = listOf(
             2000F,
             500F,
             100F,
             410F,
             120F
         )
+        view.setOnClickListener {
+            it.startAnimation(
+                AnimationUtils.loadAnimation(this, R.anim.animations)
+            )
+        }
     }
 }
